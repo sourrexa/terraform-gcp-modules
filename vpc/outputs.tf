1d5ec00c6846aa21794e79191229c741bf8147d8
Akash Kumar <akash.kumar@goapl.com>
2025-07-31 13:50:16 +0530

    udpadte

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,6 +1,4 @@
 output "network_self_link" {
   value = google_compute_network.vpc_network.self_link
 }
-output "subnetwork_self_link" {
-  value = google_compute_subnetwork.my_subnet.self_link
-}
+
