11a3d651654778741258a5c05a227c87ac9e13bc
sourrexa <sourrexa@gmail.com>
2025-07-31 14:05:49 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,3 +2,9 @@ resource "google_compute_network" "vpc_network" {
   name                    = var.name
   auto_create_subnetworks = var.auto_create_subnetworks
 }
+resource "google_compute_subnetwork" "my_subnet" {
+  name          = "my-subnet"                # Change this as needed
+  ip_cidr_range = "10.0.1.0/24"              # Specify your desired CIDR block
+  region        = var.region                 # Assumes you have a region variable
+  network       = google_compute_network.vpc_network.self_link  # Reference to your VPC
+}
