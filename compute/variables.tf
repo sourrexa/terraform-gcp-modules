edc4da7b53d5ed7eb7ee71db122d8f27d919fca2
Akash Kumar <akash.kumar@goapl.com>
2025-07-31 12:52:03 +0530

    udpadte

--- compute/variables.tf
+++ compute/variables.tf
@@ -3,7 +3,5 @@ variable "machine_type" {}
 variable "zone" {}
 variable "image" {}
 variable "network" {}
-variable "subnetwork" {
-  description = "The subnetwork to use for the instance network interface"
-  type        = string
-}
+variable "subnetwork" {}
+  
